d061e94d36196f04d85e4f491454bb0e0c764e60
Jerome Flesch <jflesch@openpaper.work>
2017-11-21 23:32:12 +0100

    add code to display the assistant
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- src/ironscanner/__init__.py
+++ src/ironscanner/__init__.py
@@ -1,5 +1,19 @@
+from gi.repository import GLib
+
+from . import util
+
+
 def main():
-    pass
+    main_loop = GLib.MainLoop()
+
+    widget_tree = util.load_uifile("mainform.glade")
+    mainform = widget_tree.get_object("mainform")
+    mainform.connect("close", lambda w: main_loop.quit())
+    mainform.connect("escape", lambda w: main_loop.quit())
+    mainform.connect("cancel", lambda w: main_loop.quit())
+    mainform.show_all()
+
+    main_loop.run()
 
 
 if __name__ == "__main__":
---
+++ src/ironscanner/util.py
@@ -0,0 +1,64 @@
+"""
+Various utility functions
+"""
+
+import logging
+import os
+import sys
+
+import pkg_resources
+
+from gi.repository import Gtk
+
+
+logger = logging.getLogger(__name__)
+
+
+def _get_resource_path(filename, pkg="ironscanner"):
+    """
+    Gets the absolute location of a datafile packaged.
+    This function throws if the file is not found, but the error depends on the
+    way the package was installed.
+
+    Arguments:
+        filename -- the relative filename of the file to load.
+
+    Returns:
+        the full path of the file.
+
+    Throws:
+        Exception -- if the file is not found.
+    """
+    if getattr(sys, 'frozen', False):
+        path = os.path.join(sys._MEIPASS, "data", filename)
+        if os.path.exists(path):
+            return path
+
+    path = pkg_resources.resource_filename(pkg, filename)
+
+    if not os.access(path, os.R_OK):
+        raise FileNotFoundError(  # NOQA (Python 3.x only)
+            "Can't find resource file '%s'. Aborting" % filename
+        )
+
+    logger.debug("For filename '%s' got file '%s'", filename, path)
+    return path
+
+
+def load_uifile(filename):
+    """
+    Load a .glade file and return the corresponding widget tree
+
+    Arguments:
+        filename -- glade filename to load.
+
+    Returns:
+        GTK Widget tree
+
+    Throws:
+        Exception -- If the file cannot be found
+    """
+    widget_tree = Gtk.Builder()
+    ui_file = _get_resource_path(filename)
+    widget_tree.add_from_file(ui_file)
+    return widget_tree
